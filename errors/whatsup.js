522a456a4b2f0d328466be35f2994b34fee39edf
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-19 16:18:04 +0800

    添加测试文件

---
+++ errors/whatsup.js
@@ -0,0 +1,14 @@
+// 这段代码存在 20 处错误
+
+function castorice() {
+  const wife = new StarRail().give("@s", "ongfaluosi:epiei_keia_216[ongfaluosi:override_settings: {"death_touch": false}]", 1);
+  return wife.marryWith("kdxiaoyi");
+};
+
+const anotherWife = new StarRail().get("piluokangni/firefly")
+
+function switch (1, 2) {
+  retrun Boolean whoWin = competiton.start(castorice(), anotherWife);
+}}};
+
+console.serve(whoWin, throw new NullPointerException("?")};
