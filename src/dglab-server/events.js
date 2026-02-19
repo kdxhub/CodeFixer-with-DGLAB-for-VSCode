11d59344dc1d11da0ee519c9d800762f29a6ff48
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-19 16:31:46 +0800

    实现代码纠错数量转为强度

--- src/dglab-server/events.js
+++ src/dglab-server/events.js
@@ -1,23 +1,54 @@
+import config from '../config.js';
+
 const vscode = require('vscode');
 const dglab = require('./main.js');
+
+/**
+ * 根据配置文件自动设置值
+ * @param {number} index 
+ * @param {string} mode 
+ * @param {number} value 
+ */
+function setStrength(index, mode, value) {
+  switch (mode) {
+    case "left":
+      dglab.power().left.set(index, value);
+      break;
+    case "right":
+      dglab.power().right.set(index, value);
+      break;
+    case "bothAvg":
+      dglab.power().right.set(index, value / 2);
+      dglab.power().left.set(index, value / 2);
+      break;
+    case "bothAll":
+      dglab.power().right.set(index, value);
+      dglab.power().left.set(index, value);
+      break;
+    default:
+      // 随机一边
+      if (Math.random() <= .5) {
+        dglab.power().right.set(index, value);
+        dglab.power().left.set(index, 0);
+      } else {
+        dglab.power().right.set(index, 0);
+        dglab.power().left.set(index, value);
+      };
+      break;
+  }
+  console.log("更新强度：", dglab.power().left.value, dglab.power().right.value);
+}
 
 function EventDiagnosticProcessor(event, context) {
   // 获取当前激活的编辑器
   const editor = vscode.window.activeTextEditor;
   if (!editor) return;
 
-  // 只处理当前文件（可选：也可以处理 e.uris 里的所有文件）
-  const currentUri = editor.document.uri;
-
-  // 获取当前文件的所有诊断信息
-  const diagnostics = vscode.languages.getDiagnostics(currentUri);
-
   // 分类统计
   let errors = 0;
   let warnings = 0;
   let infos = 0;
-
-  diagnostics.forEach(diagnostic => {
+  vscode.languages.getDiagnostics(/* 只处理当前文件（可选：也可以处理 e.uris 里的所有文件） */editor.document.uri).forEach(diagnostic => {
     switch (diagnostic.severity) {
       case vscode.DiagnosticSeverity.Error:
         errors++;
@@ -35,7 +66,22 @@ function EventDiagnosticProcessor(event, context) {
     }
   });
 
-  console.log(`当前文件 - 错误: ${errors}, 警告: ${warnings}, 提示: ${infos}`);
+  // 数量转为强度
+  let strength = 0;
+  if (errors >= 1) {
+    strength += config.code.error.first();
+    strength += config.code.error.every() * (errors - 1);
+  };
+  if (warnings >= 1) {
+    strength += config.code.warn.first();
+    strength += config.code.warn.every() * (warnings - 1);
+  };
+  if (infos >= 1) {
+    strength += config.code.info.first();
+    strength += config.code.info.every() * (infos - 1);
+  };
+  console.log("上传纠错强度：", strength);
+  setStrength(0, config.code.mode(), strength);
 }
 
 function TerminalErrorcodeProcessor(event, context) {
