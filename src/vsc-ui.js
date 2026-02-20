4e0c5a8c3e2d54e24c22278dc40cddfbc0b1c1b5
kdxiaoyi [MiPad7] <kdxiaoyi@outlook.com>
2026-02-20 12:54:46 +0800

    修复文本错误的问题

--- src/vsc-ui.js
+++ src/vsc-ui.js
@@ -26,7 +26,7 @@ const elements = {
     start: vscode.commands.registerCommand('dglab.server.start', dglab.startServer),
     stop: vscode.commands.registerCommand('dglab.server.stop', dglab.stopServer),
     pause: vscode.commands.registerCommand('dglab.server.pause', dglab.power().pause),
-    pauseForced: vscode.commands.registerCommand('dglab.server.pause.forced', dglab.power().pauseForced),
+    pauseForced: vscode.commands.registerCommand('dglab.server.pause.forced', () => { dglab.power().pauseForced("快捷键操作"); }),
     detail: vscode.commands.registerCommand('dglab.detail', dglab.switchMode),
     update: vscode.commands.registerCommand('dglab.update', updateStatusBar),
   },
@@ -73,10 +73,10 @@ function updateStatusBar() {
       break;
     case 1:
       elements.statusBar.info.text = "$(stop-circle)DGLAB：暂停";
-      elements.statusBar.info.tooltip = "当前服务已暂停，已连接${dglab.getConnected()}台客户端。\n点按以恢复。";
+      elements.statusBar.info.tooltip = `当前服务已暂停，已连接${dglab.getConnected()}台客户端。\n点按以恢复。`;
       break;
     case 2:
-      elements.statusBar.info.text = `$(heart)DGLAB ${dglab.power().left.get()} | ${dglab.power().right.get()}`;
+      elements.statusBar.info.text = `$(heart)DGLAB：${dglab.power().left.get()} | ${dglab.power().right.get()}`;
       elements.statusBar.info.tooltip = `当前服务正在运行，已连接${dglab.getConnected()}台客户端。\n两侧数字代表对应方向的强度。\n点按以暂停。`;
       break;
     case 3:
